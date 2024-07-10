337b2beed92770c35ea6fcb4ee01f95b93060c09
shahnCM <mahir.fine@hotmail.com>
2024-07-10 09:35:58 +0600

    registered routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const { request: req, response: res, next: next } = require('express')
 const { routes: fileRoutes } = require('./file')
+const { routes: uesrRoutes } = require('./user')
+const { routes: movieRoutes } = require('./movie')
+const { routes: commentRoutes } = require('./comment')
+const { routes: favoriteRoutes } = require('./favorite')
 const { handleError: handleRouteError } = require('../errors/handleError')
 
 exports.initiateRoutes = async function initiateRoutes(app) {
     fileRoutes(app)         // File Routes
+    uesrRoutes(app)
+    movieRoutes(app)
+    commentRoutes(app)
+    favoriteRoutes(app)
+
     handleRouteError(app)   // ErrorHandling
 }
